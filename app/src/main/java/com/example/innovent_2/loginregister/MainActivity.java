39faca4bb798408b8946bdf9c74c6a7e499ce5ca
jothinathaninnoventes <jothivijaymsd@gmail.com>
2016-06-10 08:48:09 +0530

    LoginToGitHub
    first Publish

---
+++ app/src/main/java/com/example/innovent_2/loginregister/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.innovent_2.loginregister;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button bLogin;
+    EditText etUsername, etPassword;
+    TextView tvRegisterLink;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        System.out.println("Inside Login acivity********************************");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bLogin = (Button) findViewById(R.id.bLogin);
+        tvRegisterLink = (TextView) findViewById(R.id.tvRegisterLink);
+
+        bLogin.setOnClickListener(this);
+        tvRegisterLink.setOnClickListener(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+    @Override
+
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.bLogin:
+                Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse( ));
+                startActivity(browserIntent);
+                break;
+            case R.id.tvRegisterLink:
+                startActivity(new Intent(this, Register.class));
+                break;
+
+
+            }
+
+
+        }
+
+}
+
+
